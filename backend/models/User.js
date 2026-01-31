0220ed41e6edfbf37fd82a9373cdbbe68135b755
danielbeinat <danielbeinat@outlook.es>
2026-01-31 19:21:14 -0300

    update

--- backend/models/User.js
+++ backend/models/User.js
@@ -39,18 +39,17 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
   try {
     const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (error) {
     console.error('Password hashing error:', error);
-    next(error);
+    throw error;
   }
 });
 
